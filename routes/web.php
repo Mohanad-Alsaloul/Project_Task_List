0c85d8396c2e9416baa448e388a8ce45a96cedfe
Mohanad-Alsaloul <mohanndeasalol@gmail.com>
2026-05-11 16:08:11 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    $name = 'Mohanad';
+        $departments =  [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    //return view('about')->with('name', $name);
+    //return view('about', ['name' => $name]);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function(){
+    $name = $_POST['name'];
+    $departments =  [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
